4a2298ebe47431d6a6fb8c20dbdf2a3995ef0758
Moymuna Jahan Sadia <jahansadia0026@gmail.com>
2025-12-23 19:05:55 +0600

    add

---
+++ New folder/asset/js/form.js
@@ -0,0 +1,27 @@
+function sendform(event) {
+  event.preventDefault();
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let password = document.getElementById(password).value;
+let dob = document.getElementById("dob").value;
+let address = document.getElementById("address").value;
+let gender = document.querySelector('input [name="gender"]:checked').value;
+let batch = document.getElementById("batch").value;
+let hobby = document.querySelectorAll('input[name="hobby"):checked').value;
+
+let output =
+'Name :' + name+ '(/n)'
+'Email :' + name+ '(/n)'
+'Password :' + name+ '(/n)'
+'Name :' + name+ '(/n)'
+'Date of Birth:' + name+ '(/n)'
+'Address :' + name+ '(/n)'
+'Gender :' + name+ '(/n)'
+'Batch:' + name+ '(/n)'
+
+
+
+
+
+}
