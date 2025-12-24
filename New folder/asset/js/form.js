e48f09dc3b7bfb5fac203e0c835df8b411c2a5a6
Moymuna Jahan Sadia <jahansadia0026@gmail.com>
2025-12-24 19:00:54 +0600

    add

--- New folder/asset/js/form.js
+++ New folder/asset/js/form.js
@@ -1,27 +1,39 @@
 function sendform(event) {
   event.preventDefault();
 
-let name = document.getElementById("name").value;
-let email = document.getElementById("email").value;
-let password = document.getElementById(password).value;
-let dob = document.getElementById("dob").value;
-let address = document.getElementById("address").value;
-let gender = document.querySelector('input [name="gender"]:checked').value;
-let batch = document.getElementById("batch").value;
-let hobby = document.querySelectorAll('input[name="hobby"):checked').value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let dob = document.getElementById("dob").value;
+  let address = document.getElementById("address").value;
+  let gender = document.querySelector('input[name="gender"]:checked');
+  let batch = document.getElementById("batch").value;
+  let hobby = document.querySelectorAll('input[name="hobby"]:checked');
+  let hobbyValue = [];
 
-let output =
-'Name :' + name+ '(/n)'
-'Email :' + name+ '(/n)'
-'Password :' + name+ '(/n)'
-'Name :' + name+ '(/n)'
-'Date of Birth:' + name+ '(/n)'
-'Address :' + name+ '(/n)'
-'Gender :' + name+ '(/n)'
-'Batch:' + name+ '(/n)'
+  for (let i = 0; i < hobby.length; i++) {
+    hobbyValue.push(hobby[i].value);
+  }
+
+  let output =
+    "Name :" + name + "\n" +
+    "Email :" +email + "\n" +
+    "Password :" + password + "\n" +
+    "Date of Birth:" +
+     dob +
+    "\n" +
+    "Address :" +
+    address +
+    "\n" +
+    "Gender :" +
+    gender.value +
+    "\n" +
+    "Batch:" +
+    batch ;
 
 
-
-
-
+  let newWindow = window.open("", "_blank");
+  newWindow.document.writeln("<pre>" + output + "</pre>");
 }
+let myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", sendform);
